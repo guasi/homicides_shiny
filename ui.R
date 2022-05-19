e233bb490188bd510c4b6ff0cbf38b0e5799bb6c
ixtapis <no-reply@ilagos.com>
2022-05-19 08:49:44 -0500

    added spinner to all plots

--- ui.R
+++ ui.R
@@ -2,42 +2,38 @@ navbarPage("WHO Homicide Estimates",
   theme = shinythemes::shinytheme("cosmo"),
   tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css")),
   collapsible = TRUE,
-  tabPanel("Interactive Charts", # Interactive Charts tab ----
-    fluidPage(
-      sidebarLayout(
-        sidebarPanel( ## Interactive Charts side -------
-          conditionalPanel(condition = "input.tabs != 'Regional' & input.tabs != 'Map'",
-            selectInput("s_region", "Region",
-                        choices = REGIONS,
-                        selected = NULL,
-                        multiple = T,
-                        selectize = F),
-            selectInput("s_country", "Country",
-                        choices = NULL,
-                        selected = NULL,
-                        multiple = T,
-                        selectize = F),
-            checkboxInput("ck_all", "Select all countries", value = F),
-            actionButton("b_clear", "clear filters", class = "btn-warning btn-sm")
-          )
-        ),
-        mainPanel( ## Interactive Charts main ---------  
-          tabsetPanel(id = "tabs",
-            tabPanel("Historical", plotOutput("plot_historical")),
-            tabPanel("Latest Year", plotOutput("plot_latest")),
-            tabPanel("By Sex", plotOutput("plot_sex")),
-            tabPanel("GDP", plotOutput("plot_gdp"),
-              hr(),
-              div(class = "center-block", style="width:300px", 
-                sliderInput("s_year", "Year",
-                            min = MIN_YR, 
-                            max = MAX_YR, 
-                            value = MIN_YR, 
-                            sep = "", animate = T)))
-          )
+  tabPanel("Interactive Charts",
+    sidebarLayout(
+      sidebarPanel( ## Interactive Charts side -------
+        selectInput("s_region", "Region",
+                    choices = REGIONS,
+                    selected = NULL,
+                    multiple = T,
+                    selectize = F),
+        selectInput("s_country", "Country",
+                    choices = NULL,
+                    selected = NULL,
+                    multiple = T,
+                    selectize = F),
+        checkboxInput("ck_all", "Select all countries", value = F),
+        actionButton("b_clear", "clear filters", class = "btn-warning btn-sm")
+      ),
+      mainPanel( ## Interactive Charts main ---------  
+        tabsetPanel(id = "tabs",
+          tabPanel("Historical", plotOutput("plot_historical")),
+          tabPanel("Latest Year", plotOutput("plot_latest")),
+          tabPanel("By Sex", plotOutput("plot_sex")),
+          tabPanel("GDP", plotOutput("plot_gdp"),
+            hr(),
+            div(class = "center-block", style="width:300px", 
+              sliderInput("s_year", "Year",
+                          min = MIN_YR, 
+                          max = MAX_YR, 
+                          value = MIN_YR, 
+                          sep = "", animate = T)))
         )
-        ## Interactive Charts end----
       )
+      ## Interactive Charts end----
     )
   ),
   tabPanel("Regional Charts",
@@ -46,7 +42,7 @@ navbarPage("WHO Homicide Estimates",
       column(6, plotOutput("plot_violin"))),
     includeMarkdown("includes/tab_regional_charts.md")
   ),
-  tabPanel("World Map", leafletOutput("map_choropleth"), class = "loading-spinner"),
+  tabPanel("World Map", leafletOutput("map_choropleth"), class = "spinner-bg"),
   tabPanel("About", includeMarkdown("includes/tab_about.md")),
   tags$footer(includeHTML("includes/footer.html"))
 )
